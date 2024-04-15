1373518d1100feecfc9a626a179cb86f96d9a13c
Tristen <tristen.perez@ucdenver.edu>
2024-04-14 19:57:38 -0600

    Updated Files

--- Movies.js
+++ Movies.js
@@ -27,14 +27,32 @@ mongoose.connect(process.env.DB);
 
 
 //Movie Collection Schema to store movie information:
-  const MovieSchema = new mongoose.Schema({
+const MovieSchema = new mongoose.Schema({
     title: { type: String, required: true, index: true },
-    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
-    genre: { type: String, enum: genres },
-    actors: [ActorSchema],
+    releaseDate: Date,
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+    },
+    actors: [{
+      actorName: String,
+      characterName: String,
+    }],
     imageUrl: String,
-    // rest of your fields
   });
+
+
+//Movie Collection Schema to store movie information:
+  // const MovieSchema = new mongoose.Schema({
+  //   title: { type: String, required: true, index: true },
+  //   releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+  //   genre: { type: String, enum: genres },
+  //   actors: [ActorSchema],
+  //   imageUrl: String,
+  //   // rest of your fields
+  // });
   
 
 // return the model
